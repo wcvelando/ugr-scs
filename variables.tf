5441154f68216fa94cf2ab3fff86397ed4228561
Walter Velando <walter.velando@gmail.com>
2025-11-05 15:39:06 -0300

    Updated variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
-variable "prefix" { type = string  default = "ugr-sec" }
+variable "prefix" { type = string  default = "ugr-scs" }
 variable "location" { type = string  default = "eastus" }
 variable "resource_group_name" { type = string  default = null }
